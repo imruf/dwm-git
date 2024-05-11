fa30d125119737c1cb6c272b42f570348a0e59de
imrufbd <imrufbd@gmail.com>
2024-05-11 13:46:37 +0600

    movethrow patch

--- dwm.c
+++ dwm.c
@@ -70,6 +70,7 @@ enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
 enum { WMProtocols, WMDelete, WMState, WMTakeFocus, WMLast }; /* default atoms */
 enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
        ClkClientWin, ClkRootWin, ClkLast }; /* clicks */
+enum { DIR_N, DIR_W, DIR_C, DIR_E, DIR_S, }; /* coordinates for movethrow */
 
 typedef union {
 	int i;
@@ -193,6 +194,7 @@ static void motionnotify(XEvent *e);
 static void moveresize(const Arg *arg);
 static void moveresizeedge(const Arg *arg);
 static void movemouse(const Arg *arg);
+static void movethrow(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *c);
 static void propertynotify(XEvent *e);
@@ -1443,6 +1445,46 @@ moveresizeedge(const Arg *arg) {
 		if ((msx + nmx) > c->x && (msy + nmy) > c->y)
 			XWarpPointer(dpy, None, None, 0, 0, 0, 0, nmx, nmy);
 	}
+}
+
+void
+movethrow(const Arg *arg)
+{
+	Client *c;
+	int nh, nw, nx, ny;
+	c = selmon->sel;
+	if (!c)
+		return;
+	if (selmon->lt[selmon->sellt]->arrange && !c->isfloating)
+		togglefloating(NULL);
+	nw = c->w;
+	nh = c->h;
+	switch(arg->ui) {
+		case DIR_N:
+			nx = c->x;
+			ny = selmon->wy;
+			break;
+		case DIR_E:
+			nx = selmon->wx + selmon->ww - c->w - c->bw*2;
+			ny = c->y;
+			break;
+		case DIR_S:
+			nx = c->x;
+			ny = selmon->wy + selmon->wh - c->h - c->bw*2;
+			break;
+		case DIR_W:
+			nx = selmon->wx;
+			ny = c->y;
+			break;
+		case DIR_C:
+			nx = selmon->wx + ((selmon->ww - c->w - c->bw*2) / 2);
+			ny = selmon->wy + ((selmon->wh - c->h - c->bw*2) / 2);
+			break;
+		default:
+			return;
+	}
+	resize(c, nx, ny, nw, nh, True);
+	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, nw/2, nh/2);
 }
 
 Client *
