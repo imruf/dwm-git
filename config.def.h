1f78e2756c12942653781485cb449b61e7cd745c
imrufbd <imrufbd@gmail.com>
2024-05-11 14:31:55 +0600

    font fix

--- config.def.h
+++ config.def.h
@@ -10,8 +10,14 @@ static const unsigned int snap      = 20;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:bold:pixelsize=12:antialias=true:autohint=true", "UbuntuMono Nerd Font:size=10:antialias=true:autohint=true", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static const char dmenufont[]       = { "mono:pixelsize=14:antialias=true:autohint=true" };
+static const char Normfg[]      = "#ecc48d"; /* layout indicator and status text color #acb4c2 #f5f5dc #d79921 #ebcb8b #999999 #bf616a */
+static const char Normbg[]      = "#011627"; /* border color #011627 #011111 #111111 #073642 #222222*/
+static const char Normbd[]      = "#444444";
+static const char Selbg[]       = "#000000"; /* #000000 #005577*/
+static const char Selfg[]       = "#eeeeee"; /* tag and windwo name color #eeeeee */
+static const char Selbd[]       = "#770000"; /* bar color #111111 #005577 #0a0f14 #ff0000 #ffff00 #ffffff #fea64c #282c34 #d7d7d7 #02143f */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -21,8 +27,10 @@ static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] =	 { Normfg, Normbg, Normbd },
+	[SchemeSel]  =	 { Selfg, Selbg, Selbd },
+/*	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
+/*	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
